2fa3fec0fcacb2df48e956dcf2962140ae6999d8
Matthew Meades <mm16507@it075675.wks.bris.ac.uk>
2019-10-09 12:03:28 +0100

    MonteCarlo Complete

--- Week2/2.6/MonteCarlo.c
+++ Week2/2.6/MonteCarlo.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #define NDEBUG
 #include <assert.h>
+#define ACCURACY 0.0000001
 
 void test(void);
 int inCircle(float x, float y);
@@ -11,22 +12,22 @@ int inCircle(float x, float y);
 int main(void) {
   /* ha is the number of darts inside the square, hc is the number of darts in
   // the circle */
-  int i, ha = 0, hc = 0;
-  float x, y;
+  int i = 0, ha = 0, hc = 0;
+  float x, y, approximation=0.0;
   srand(time(NULL));
-
-  for (i=0; i < 10000; i++) {
-    x = (rand() % 2) - 1;
-    y = (rand() % 2) - 1;
-    printf("x is %f, y is %f\n", x, y);
+  while (fabs(approximation - 3.14159265) > ACCURACY) {
+    i++;
+    x = 2*(float)rand()/(float)(RAND_MAX)-1;
+    y = 2*(float)rand()/(float)(RAND_MAX)-1;
     if (inCircle(x,y)) {
       hc++;
-      ha++;
-    } else {
-      ha++;
     }
+    ha++;
+    approximation = (float)4*(float)hc/(float)ha;
+    printf("%d: Approximation to pi = %0.10f\n", i, approximation);
   }
-
+  approximation = (float)4*(float)hc/(float)ha;
+  printf("%d: Approximation to pi = %0.10f\n", i, approximation);
   return 0;
 }
 
@@ -43,5 +44,4 @@ void test(void) {
   assert(inCircle(50,40) == 0);
   assert(inCircle(0.67, 0.34) == 1);
   assert(inCircle(-0.67, -0.34) == 1);
-
 }
