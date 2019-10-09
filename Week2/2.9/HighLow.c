b0dff4a309ed2bba2834b6b2ecffb09d1f89060f
Matthew Meades <mm16507@it075819.wks.bris.ac.uk>
2019-10-09 16:51:28 +0100

    Week 2 complete

--- Week2/2.9/HighLow.c
+++ Week2/2.9/HighLow.c
@@ -1,10 +1,55 @@
 #include <stdio.h>
+#define NDEBUG
 #include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 void test(void);
+int checkGuess(int guess, int random);
 
 int main(void) {
-  int guess;
+  int guess, random, tries = 9;
+  srand(time(NULL));
+  test();
+  random = rand() % 1000;
+  printf("Random number is %d\n", random);
 
+  printf("Guess the number I'm thinking of between 1 and 1000: ");
+  scanf("%d", &guess);
+  while (tries) {
+    if (checkGuess(guess, random) == 0) {
+      scanf("%d", &guess);
+      tries--;
+      printf("You have %d tries left!\n", tries);
+    }
+    else {
+      tries = 0;
+    }
+  }
   return 0;
+}
+
+int checkGuess(int guess, int random) {
+  if (guess > random) {
+    printf("Too high! Try again: ");
+    return 0;
+  }
+  else if (guess < random) {
+    printf("Too low! Try again: ");
+    return 0;
+  }
+  else if (guess == random) {
+    printf("Congratulations! %d was the right answer!\n", guess);
+    return 1;
+  }
+  else {
+    printf("NOT A VALID GUESS, TRY AGAIN: ");
+    return 0;
+  }
+}
+
+void test(void) {
+  assert(checkGuess(10, 10) == 1);
+  assert(checkGuess(10, 9) == 0);
 }
