3234cba3feba40bec597fa2081e7cd99b0df97e0
Matthew Meades <mm16507@it075718.wks.bris.ac.uk>
2019-10-11 13:51:54 +0100

    Started 3.3

---
+++ Week3/3.3/Rule110.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <assert.h>
+#define ARRAYWIDTH 50
+
+enum input {central, random}
+typedef enum input input
+
+void test(void);
+
+int main(void) {
+  int currentArray[ARRAYWIDTH], newArray[ARRAYWIDTH], userInput = 1;
+
+  return 0;
+}
+
+void nextNumber(int *currentArray, int *newArray, int i) {
+
+}
+
+void initialiseArray(int *currentArray, int userInput) {
+  if (userInput == ) {
+
+  }
+}
