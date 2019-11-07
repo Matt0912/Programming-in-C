fbce101b2befa639c8c3086f7d1878cb570e5b82
Matthew Meades <mm16507@it075906.wks.bris.ac.uk>
2019-11-07 18:00:08 +0000

    Added 5.3, given up on 5.2

--- Week5/5.2/unlock.c
+++ Week5/5.2/unlock.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+/* PROGRAM PARAMETERS */
 #define GRIDSIZE 3
 #define PATHLENGTH 9
+#define STARTX 0
+#define STARTY 0
 
 enum bool {FAIL, SUCCESS};
+/* Moves named as 1, 2, 3, 4 */
+enum moves {START, UP, RIGHT, DOWN, LEFT};
+
+typedef struct validPaths {
+  int path[PATHLENGTH];
+  int pathNumber;
+  struct validPaths *next;
+} validPaths;
 
 void test(void);
-int explore(int x, int y, int grid[GRIDSIZE][GRIDSIZE], int num);
+int explore(int x, int y, int grid[GRIDSIZE][GRIDSIZE], int num, int direction, validPaths *sp);
 void printArray(int grid[GRIDSIZE][GRIDSIZE]);
 
 
 int main(void) {
-  int startx = 0, starty = 0;
+  validPaths head, *sp = &head;
+  int i, startx = STARTX, starty = STARTY, complete = FAIL;
   int grid[GRIDSIZE][GRIDSIZE] = {{0}};
+  head.next = NULL;
 
-  if (explore(startx, starty, grid, 1)) {
-    printArray(grid);
-  }
-  else {
-    printf("Not a valid route\n");
-  }
+  while (complete = FAIL) {
+    if (explore(startx, starty, grid, 0, START, sp)) {
+      printArray(grid);
+    }
 
+
+  for (i=0; i < PATHLENGTH; i++) {
+    printf("%d ", sp->path[i]);
+  }
 
   return 0;
 }
@@ -31,39 +46,43 @@ void test(void) {
 }
 
 
-int explore(int x, int y, int grid[GRIDSIZE][GRIDSIZE], int num) {
-
+int explore(int x, int y, int grid[GRIDSIZE][GRIDSIZE], int num, int direction, validPaths *sp) {
   if (x >= GRIDSIZE || y >= GRIDSIZE || x < 0 || y < 0) {
     return FAIL;
   }
 
+  printf("%d \n", sp->path[num-1]);
+  printf("_____________________\n");
+
   if (grid[y][x] == 0) {
+    sp->path[num] = direction;
+    num += 1;
     grid[y][x] = num;
   } else {
     return FAIL;
   }
 
   /* IF WE CAN GO RIGHT */
-  if (explore(x+1, y, grid, num+1)) {
+  if (explore(x+1, y, grid, num, RIGHT, sp)) {
     return SUCCESS;
   }
   /* IF WE CAN GO UP */
-  if (explore(x, y+1, grid, num+1)) {
+  if (explore(x, y-1, grid, num, UP, sp)) {
     return SUCCESS;
   }
   /* IF WE CAN GO LEFT */
-  if (explore(x-1, y, grid, num+1)) {
+  if (explore(x-1, y, grid, num, LEFT, sp)) {
     return SUCCESS;
   }
   /* IF WE CAN GO DOWN */
-  if (explore(x, y-1, grid, num+1)) {
+  if (explore(x, y+1, grid, num, DOWN, sp)) {
     return SUCCESS;
   }
   if (num == PATHLENGTH) {
     return SUCCESS;
-  } else {
-    return FAIL;
   }
+
+  return FAIL;
 }
 
 void printArray(int grid[GRIDSIZE][GRIDSIZE]) {
