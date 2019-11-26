d212630d2d45bd102981783f74e0532498615cb5
Matthew Meades <mm16507@it075869.wks.bris.ac.uk>
2019-11-26 16:48:52 +0000

    fixed tree depth, completed compare trees 9.2

--- Week9/9.2/compTrees.c
+++ Week9/9.2/compTrees.c
@@ -6,6 +6,8 @@
 
 #define STRSIZE 5000
 
+enum bool {FALSE, TRUE};
+
 typedef struct node {
   char c;
   struct node *left;
@@ -15,22 +17,40 @@ typedef struct node {
 Node *MakeNode(char c);
 void InsertRandom(Node *t, Node *n);
 void findDepth(Node *head, int currDepth, int *maxDepth);
+int compareTrees(Node *head1, Node *head2, int *count);
 char *PrintTree(Node *t);
 
 int main(void)
 {
   char c;
-  Node *head = MakeNode('A');
-  Node *n;
-  int maxDepth = 0;
+  Node *head1 = MakeNode('A');
+  Node *head2 = MakeNode('A');
+  Node *n1, *n2;
+  int maxDepth1 = 0, maxDepth2 = 0, count = 0;
   srand(time(NULL));
   for (c = 'B'; c < 'G'; c++){
-    n = MakeNode(c);
-    InsertRandom(head, n);
+    n1 = MakeNode(c);
+    n2 = MakeNode(c);
+    InsertRandom(head1, n1);
+    InsertRandom(head2, n2);
   }
-  findDepth(head, 1, &maxDepth);
-  printf("%s\n", PrintTree(head));
-  fprintf(stdout, "Max depth = %d\n", maxDepth);
+  findDepth(head1, 1, &maxDepth1);
+  findDepth(head2, 1, &maxDepth2);
+  printf("%s\n", PrintTree(head1));
+  printf("%s\n", PrintTree(head2));
+
+  if (maxDepth1 == maxDepth2) {
+    if (compareTrees(head1, head2, &count)) {
+      fprintf(stdout, "TREES ARE IDENTICAL\n");
+    }
+    else {
+      fprintf(stdout, "TREES ARE NOT IDENTICAL\n");
+    }
+  }
+  else {
+    fprintf(stdout, "TREES ARE NOT IDENTICAL\n");
+  }
+
   return 0;
 }
 
@@ -50,7 +70,7 @@ void InsertRandom(Node *t, Node *n)
       t->left = n;
     }
     else {
-    InsertRandom(t->left, n);
+      InsertRandom(t->left, n);
     }
   }
   /* Right */
@@ -59,7 +79,7 @@ void InsertRandom(Node *t, Node *n)
       t->right = n;
     }
     else {
-    InsertRandom(t->right, n);
+      InsertRandom(t->right, n);
     }
   }
 }
@@ -73,6 +93,34 @@ void findDepth(Node *head, int currDepth, int *maxDepth) {
   }
   if (currDepth > *maxDepth) {
     *maxDepth = currDepth;
+  }
+}
+
+int compareTrees(Node *head1, Node *head2, int *count) {
+  if (*count > 0) {
+    return FALSE;
+  }
+
+  if (head1 == head2 && head2 == NULL) {
+    return TRUE;
+  } else if (head1 == NULL || head2 == NULL){
+    (*count)++;
+    return FALSE;
+  }
+
+  if (head1->c == head2->c) {
+    compareTrees(head1->left, head2->left, count);
+    compareTrees(head1->right, head2->right, count);
+  }
+  else {
+    (*count)++;
+    return FALSE;
+  }
+
+  if (*count == 0) {
+    return TRUE;
+  } else {
+    return FALSE;
   }
 }
 
