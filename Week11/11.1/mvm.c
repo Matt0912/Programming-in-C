1f2221e37041f8f29426730ce3972bbbd11724f9
Matthew Meades <mm16507@it075906.wks.bris.ac.uk>
2019-12-10 17:13:40 +0000

    11.1 memleaks almost fixed

--- Week11/11.1/mvm.c
+++ Week11/11.1/mvm.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "mvm.h"
 
-#define MAXSTRSIZE 150
+#define MAXSTRSIZE 50
+#define MAXOUTPUTSTR 1000
+#define LISTSIZE 2
 
 enum bool {FALSE, TRUE};
 
@@ -34,8 +37,10 @@ void mvm_insert(mvm* m, char* key, char* data) {
     free(cell);
   }
   else {
-    cell->key = key;
-    cell->data = data;
+    cell->key = (char *)malloc(sizeof(char)*MAXSTRSIZE);
+    strcpy(cell->key, key);
+    cell->data = (char *)malloc(sizeof(char)*MAXSTRSIZE);
+    strcpy(cell->data, data);
     cell->next = m->head;
     m->head = cell;
     (m->numkeys)++;
@@ -44,11 +49,16 @@ void mvm_insert(mvm* m, char* key, char* data) {
 
 /* Store list as a string "[key](value) [key](value) " etc.  */
 char* mvm_print(mvm* m) {
-  char *output = (char *)calloc(1, MAXSTRSIZE*sizeof(char));
-  char str[MAXSTRSIZE];
+  char *output = (char *)calloc(1, MAXOUTPUTSTR*sizeof(char));
+  char str[MAXSTRSIZE*3];
   mvmcell* ptr = m->head;
+  int i = 1;
   while (ptr != NULL) {
     sprintf(str, "[%s](%s) ", ptr->key, ptr->data);
+    if (strlen(output) > MAXOUTPUTSTR*0.8*i) {
+      output = (char*)realloc(output, sizeof(char)*MAXOUTPUTSTR*i);
+      i++;
+    }
     strcat(output, str);
     ptr = ptr->next;
   }
@@ -57,29 +67,33 @@ char* mvm_print(mvm* m) {
 
 /* Remove one key/value */
 void mvm_delete(mvm* m, char* key) {
-  mvmcell* ptr;
+  mvmcell *curr, *prev;
   int i = 0;
   if (m != NULL && key != NULL) {
-    ptr = m->head;
+    curr = m->head;
+    prev = NULL;
     /* If head = cell to be deleted */
-    if (strcmp(ptr->key, key) == 0) {
+    if (strcmp(curr->key, key) == 0) {
       m->head = m->head->next;
-      free(ptr);
+      free(curr->key);
+      free(curr->data);
+      free(curr);
       (m->numkeys)--;
     }
     /* Else search through list */
     else {
       while (i < m->numkeys - 1) {
-        if (strcmp(ptr->next->key, key) == 0) {
-          free(ptr->next);
-          ptr->next = ptr->next->next;
+        prev = curr;
+        curr = curr->next;
+        if (strcmp(curr->key, key) == 0) {
+          prev->next = curr->next;
+          free(curr->key);
+          free(curr->data);
+          free(curr);
           (m->numkeys)--;
           i = m->numkeys;
         }
-        else {
-          ptr = ptr->next;
-          i++;
-        }
+        i++;
       }
     }
   }
@@ -105,28 +119,36 @@ char* mvm_search(mvm* m, char* key) {
 /* Return *argv[] list of pointers to all values stored
 // with key, n is the number of values */
 char** mvm_multisearch(mvm* m, char* key, int* n) {
-  char **argv = (char **)malloc(sizeof(char*));
+  char **argv = (char **)calloc(1, LISTSIZE*sizeof(char**));
   mvmcell* ptr = m->head;
-  int i = 0, j = 0;
-  do {
+  int i = 1, j = 0;
+  while (j < m->numkeys) {
     if (strcmp(ptr->key, key) == 0) {
-      argv[i] = ptr->data;
+      if (*n > LISTSIZE * i * 0.8) {
+        printf("n = %d\n", *n);
+        argv = (char **)realloc(argv, sizeof(char**)*LISTSIZE*i);
+        i++;
+      }
+      argv[*n] = ptr->data;
       (*n)++;
-      i++;
     }
     ptr = ptr->next;
     j++;
-  } while (j < m->numkeys);
+  }
   return argv;
 }
+
 /* Free & set p to NULL */
 void mvm_free(mvm** p) {
   mvm* m = *p;
   mvmcell* ptr = m->head;
   while (m->head != NULL) {
     m->head = m->head->next;
+    free(ptr->key);
+    free(ptr->data);
     free(ptr);
     ptr = m->head;
   }
+  free(m);
   *p = NULL;
 }
